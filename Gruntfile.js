5e7f55d6c654267b85c53fdc9aad938a132e2a30
Reinier Ladan <reinierl@gmail.com>
2014-03-05 16:45:47 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			build: {
+		        files: {
+		            './public/css/site.css': './src/site.scss'
+		        }
+		    }
+	    },
+		watch: {
+			build: {
+				files: ['src/*.scss','src/*.html','src/html_includes/*.html'],
+				tasks: ['sass','cssmin','bake']
+			},
+		},
+		cssmin: {
+			combine: {
+				files: {
+					'./public/css/full.min.css': ['./bower_components/normalize.css/normalize.css','./public/css/site.css']
+				}
+			}
+		},
+		bake: {
+	        build: {
+	            files: [{
+			        expand: true,
+			        cwd: './src',
+			        src: ['*.html'],
+			        dest: './public',
+			        ext: '.html'
+				}]
+	        },
+	    },
+	});
+
+	grunt.registerTask('default',['watch:build']);
+	grunt.registerTask('render',['sass','cssmin','bake']);
+};
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Boilerplate
+
+[My](http://reinier.io) boilerplate for new projects. Includes [Grunt](http://gruntjs.com), [Bower](http://bower.io), [Bourbon](http://bourbon.io), [Neat](http://neat.bourbon.io) and [Bitters](http://bitters.bourbon.io).
+
+## Setup
+```
+bower install
+cd src/sass/
+bourbon install
+neat install
+bitters install
+cd ../../
+npm install
+```
